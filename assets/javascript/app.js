dfec9c90cc3cb1be3ac0ddb8de4a5208a27c0711
Jarrod <jwmcenroe@gmail.com>
2017-09-29 22:36:44 -0700

    Added submit, timer and hide functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,45 +5,43 @@ $('document').ready(function(){
 	var incorrectQuestions = 0;
 	var unansweredQuestions = 0;
 	var counter;
-	var timeRemaining;
+	var timeLeft;
 
 	/* click button to begin */
 	$(".start").on("click",function(){
-		/* hide start button, show questions */
+		/* hide start screen, show questions */
 		$(".sectionWrap").removeClass("hide");
-		$(".start").addClass("hide");
+		$(".start, .header").addClass("hide");
 
-		/* one for timesUp counter, the other for regular counter */
-		timeRemaining = 45;
-		counter = setTimeout(function(){timeUp()},45000);
-		/* above code runs timeUp function at 45 seconds */
+		/* one for timesUp counter, the other for regular counter for 45 seconds*/
+		timeLeft = 45;
+		counter = setTimeout(function(){timesUp()},45000);
 
-		/* every second, run countDownDisplay function */
+		/* countDownDisplay function runs every second*/
 	    secondsInterval = setInterval(countDownDisplay,1000);
 
-	    /* if time remaining is greater than 0, decrement time remaining, push to html. */
+	    /* if time remaining is greater than 0, decrease time remaining, push to html. */
 	    function countDownDisplay (){
-	    	if (timeRemaining>0){
-			  	timeRemaining--;
-			  	$(".timeDisplay").html(timeRemaining + " Seconds Remaining.");
+	    	if (timeLeft>0){
+			  	timeLeft--;
+			  	$(".timer").html(timeLeft + " Seconds Remaining");
 			    }
 	    	};
 
 		function timesUp(){
-			alert("Time's up!");
+			$(".Mainsection").removeClass("hide");
 			triviaValues();
 			logResults();
-			$(".testAnswerButton").addClass("hide");
+			$("sectionWrap .done").addClass("hide");
 		};
 
-	}); /* end startbutton click */
+	}); /* end start button click */
 
-	/* run loop for total questions. */
+	/* loop for questions */
 	function triviaValues(){
 		for(i=0; i<8 ;i++){
-			/* index of each a1, a2, a3, etc. */
+			/* index of each a1, a2, a3, etc */
 			var userInput = $('input[name="a'+i+'"]:checked').value;
-			console.log(userInput);
 			/* add to score counters */
 			if(userInput==="true"){
 				correctQuestions+=1;
@@ -56,30 +54,30 @@ $('document').ready(function(){
 	};
 
 	function logResults(){
-		/* clear time counters, stop timed functions from running. */
+		/* clear time counters, stop timers from running */
 		clearInterval(counter);
-		timeRemaining=0;
-		/* push values to html */
-		$(".timeDisplay").html("<p> You got " + correctQuestions + " right and " + incorrectQuestions + " wrong. " + unansweredQuestions + " question(s) remained unanswered.</p>");
+		timeLeft=0;
+		/* insert values to html */
+		$(".timer").html("<p> You got " + correctQuestions + " right and " + incorrectQuestions + " wrong. " + unansweredQuestions + " question(s) remained unanswered.</p>");
 	};
-	/* on click, get values, return values, and hide own button */
+	/* click 'Done', get values, return values, and hide trivia question and timer page */
 	$(".done").on("click",function(){
-		testValues();
+		triviaValues();
 		logResults();
-		$(".done").addClass("hide");
+		$(".done .sectionWrap").addClass("hide");
 	});
 
-	/* on click, restore defaults. */
+	/* on click, restore defaults */
 	$(".reset").on("click",function(){
 		correctQuestions = 0;
 		incorrectQuestions = 0;
 		unansweredQuestions = 0;
 		clearInterval(counter);
 		clearInterval(secondsInterval);
-		timeRemaining = 0;
+		timeLeft = 0;
 		$(".sectionWrap").addClass("hide");
 		$(".start").removeClass("hide");
-		$(".timeDisplay").html("");
+		$(".timer").html("");
 		$(".done").removeClass("hide");
 	});
 
